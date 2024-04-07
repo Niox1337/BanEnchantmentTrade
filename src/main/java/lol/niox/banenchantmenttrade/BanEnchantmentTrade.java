f7c70be2a37e0edfb78fd3880d27288385c17a41
Zhixiang Feng <2616718f@student.gla.ac.uk>
2024-04-07 17:53:04 +0100

    test functionality

--- src/main/java/lol/niox/banenchantmenttrade/BanEnchantmentTrade.java
+++ src/main/java/lol/niox/banenchantmenttrade/BanEnchantmentTrade.java
@@ -1,17 +1,25 @@
 package lol.niox.banenchantmenttrade;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.entity.VillagerAcquireTradeEvent;
 
-public final class BanEnchantmentTrade extends JavaPlugin {
+public final class BanEnchantmentTrade extends JavaPlugin implements org.bukkit.event.Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
+        System.out.println("BanEnchantmentTrade has been enabled!");
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+
+    @EventHandler
+    public void onVillagerAcquireTrade(VillagerAcquireTradeEvent event) {
+        System.out.println(event.getRecipe().getResult().getType());
+        if (event.getRecipe().getResult().getType().toString().contains("ENCHANTED")) {
+            event.setCancelled(true);
+        }
     }
+
 }
